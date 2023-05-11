69df3184cbefb890239e082cd4955c8725e1cfda
Gab <g.mazeau@spinalcom.com>
2023-05-11 15:32:39 +0200

    new vue client

--- vue-client/src/modules/logs.js
+++ vue-client/src/modules/logs.js
@@ -1,19 +1,19 @@
 /*
  * Copyright 2023 SpinalCom - www.spinalcom.com
- * 
+ *
  * This file is part of SpinalCore.
- * 
+ *
  * Please read all of the following terms and conditions
  * of the Free Software license Agreement ("Agreement")
  * carefully.
- * 
+ *
  * This Agreement is a legally binding contract between
  * the Licensee (as defined below) and SpinalCom that
  * sets forth the terms and conditions that govern your
  * use of the Program. By installing and/or using the
  * Program, you agree to abide by all the terms and
  * conditions stated or referenced herein.
- * 
+ *
  * If you do not agree to abide by these terms and
  * conditions, do not demonstrate your acceptance and do
  * not install or use the Program.
@@ -22,30 +22,32 @@
  * <http://resources.spinalcom.com/licenses.pdf>.
  */
 
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
-import store from './store'
-import router from './router';
-import vuetify from '@/plugins/vuetify'; // path to vuetify export
-import VueCompositionAPI from '@vue/composition-api';
-import Vuelidate from 'vuelidate';
-Vue.use(Vuelidate);
-Vue.use(VueCompositionAPI);
-Vue.config.productionTip = false;
 
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  store,
-  components: { App },
-  template: '<App/>',
-  vuetify,
-  validations: {},
-  // mounted() {
-  //   this.$router.replace('/login');
-  // },
-});
+const instanceAxios = require("../services/axiosConfig");
+export default {
+    namespaced: true,
+    state: {
+        data: [],
+        loglist: [],
+    },
+    getters: {
+        getlogList: state => state.loglist,
+    },
+    actions: {
+        async getLogs({ commit }) {
+            
+            const rep = await instanceAxios.instanceAxios.get("/logs", {
+                headers: {
+                  "Content-Type": "application/json",
+                  "x-access-token": localStorage.getItem("token")
+                }
+              });
+            commit("setlogList", rep.data);
+        },
+    },
+    mutations: {
+        setlogList: (state, loglist) => (
+            state.loglist = loglist
+        ),
+    },
+}
--- vue-client/src/services/axiosConfig.js
+++ vue-client/src/services/axiosConfig.js
@@ -24,7 +24,8 @@
 // const axios = require('axios');
 import axios from "axios";
 export const instanceAxios = axios.create({
-  baseURL: 'http://localhost:4040/',
+  baseURL: 'http://localhost:9060/',
+  // baseURL: '/',
   timeout: 1000,
   headers: { 'X-Custom-Header': 'foobar' },
 });
@@ -34,6 +35,7 @@ import router from "../router";
 instanceAxios.interceptors.response.use((response) => {
   return response;
 }, (error) => {
+  
   if (error.response.status === 401) {
     localStorage.removeItem('token');
     router.push("/Login");
