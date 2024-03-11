52a6832fbce569ff36203488716adaaee40bbc25
mbt-tech <mbaradji221@outlook.fr>
2024-03-11 16:02:44 +0100

    add monitoring lib

--- src/config.ts
+++ src/config.ts
@@ -24,24 +24,15 @@
 
 const config = {
   spinalConnector: {
-    user: process.env.SPINAL_USER_ID || 168, // user id
-<<<<<<< Updated upstream
-    password: process.env.SPINAL_PASSWORD ||
-      "3HNvC6fftqHz", // user password
-    host: process.env.SPINALHUB_IP || "localhost", // can be an ip address
+    organName : "Spinal-organ-auth",
+    user: process.env.SPINAL_USER_ID , // user id
+    password: process.env.SPINAL_PASSWORD , // user password
+    host: process.env.SPINALHUB_IP, // can be an ip address
     port: process.env.SPINALHUB_PORT || 7777 // port
   },
   api: {
     host: process.env.REQUESTS_HOST || 'localhost', // internal port
     port: 4040 // internal port
-=======
-    password: process.env.SPINAL_PASSWORD || 'ApgL69i7', // user password
-    host: process.env.SPINALHUB_IP || 'localhost', // can be an ip address
-    port: process.env.SPINALHUB_PORT || 9050, // port
-  },
-  api: {
-    port: process.env.REQUESTS_PORT || 9060, // internal port
->>>>>>> Stashed changes
   },
   file: {
     // path to a digital twin in spinalhub filesystem
--- src/spinalMiddleware.ts
+++ src/spinalMiddleware.ts
@@ -30,6 +30,8 @@ const { SpinalServiceUser } = require('spinal-service-user');
 import { store } from './utilities/utilitiesFunctions'
 
 import config from './config'
+import ConfigFile from "spinal-lib-organ-monitoring";
+
 
 class SpinalMiddleware {
   static instance: SpinalMiddleware = null;
@@ -70,8 +72,9 @@ class SpinalMiddleware {
       reject("IS NOT ABLE TO CONNECT TO HUB")
     })
   }
-  onLoadSuccess(resolve: () => void, graph: SpinalGraph<any>) {
-    SpinalGraphService.setGraph(graph);
+  async onLoadSuccess(resolve: () => void, graph: SpinalGraph<any>) {
+    await SpinalGraphService.setGraph(graph);
+    await ConfigFile.init(this.conn, config.spinalConnector.organName, "Spinal-organ-auth", config.spinalConnector.host, parseInt(config.spinalConnector.port as string));
     resolve();
   }
 
